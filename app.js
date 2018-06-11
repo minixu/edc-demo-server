74df07966572ff799618f6d022e9427df7980124
minixu <307287103@qq.com>
2018-06-11 10:33:30 +0800

    1

---
+++ app.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+app.get('*', function(req, res) {
+    const html = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf-8')
+    res.send(html)
+})
+app.listen(8082);
